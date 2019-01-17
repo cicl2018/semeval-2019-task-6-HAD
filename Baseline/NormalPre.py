ff73dec6a365e002b948ce0f5a1a709d6d882b0f
Himanshu Bansal <himanshu.bansal@kelltontech.com>
2019-01-17 16:10:59 +0100

    GPU

--- Baseline/NormalPre.py
+++ Baseline/NormalPre.py
@@ -179,6 +179,10 @@ def pre_processing(text):
 	text = text.replace("=", " ")
 	text = text.replace("$", "")
 	text = text.replace("£", "")
+	text = text.split("!")
+	text = " ".join(text)
+	text = text.split("?")
+	text = " ".join(text)
 	text = re.sub(r"^[^\d]$", "", text)
 	# parse hashtags
 	if not re.match(r"#[^\s]+", text) == None:
