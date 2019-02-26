9b112a9cddbdb10f7ef7e679a23414e79376bd22
Himanshu Bansal <himanshu.bansal@kelltontech.com>
2019-02-26 07:38:06 +0100

    Cleanup

--- Task B/p.py
+++
@@ -1,5 +0,0 @@
-with open('training1.tsv', 'w+') as w:
-    with open('training.tsv', 'r+') as r:
-        for line in r.readlines():
-            if  line.replace("\n", "").split("\t")[2] == "OFF":
-                w.write(line)
